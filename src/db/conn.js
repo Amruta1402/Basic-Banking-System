d9e81a2834fdeadd12a1469ffadfe5e60c9e7d43
Amruta Talreja <ammutalreja14@gmail.com>
2021-07-12 18:19:50 +0530

    connecting to mongodb atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 //connection
-mongoose.connect('mongodb://localhost:27017/bankdb',{
+mongoose.connect('mongodb+srv://Amruta-1402:ammu1402@cluster0.6bxpm.mongodb.net/bankdb',{
     useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
